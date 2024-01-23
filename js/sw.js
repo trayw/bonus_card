92aeb71706a202b5e21355fa0f48d140cba08af0
trayw <130303970+trayw@users.noreply.github.com>
2024-01-23 12:13:50 +0600

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'my-pwa-cache';
+const CACHE_FILES = [
+  '/',
+  '/index.html',
+  '/css/normalize.css',
+  '/css/main.css'
+  // '/script.js',
+  // '/icon.png'
+];
+ 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+ 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+ 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
